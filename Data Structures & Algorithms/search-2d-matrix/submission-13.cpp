2d5604039c5403d048a397f1f501ad4f485839be
AllenDiiaz <88265742+AllenDiiaz@users.noreply.github.com>
2026-07-19 21:54:12 +0800

    Add: search-2d-matrix - submission-13

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-13.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int num_ele = row * col;
+        int left = 0;
+        int right = num_ele - 1;
+
+        while (left<=right)
+        {
+            int mid = left + (right-left) / 2;
+
+            int loc_row = mid / col;
+            int loc_col = mid % col;
+
+            if (matrix[loc_row][loc_col] == target) return true;
+
+            else if (matrix[loc_row][loc_col] > target) right = mid - 1;
+
+            else if (matrix[loc_row][loc_col] < target) left = mid + 1;
+        }
+
+        return false;
+        
+    }
+};
