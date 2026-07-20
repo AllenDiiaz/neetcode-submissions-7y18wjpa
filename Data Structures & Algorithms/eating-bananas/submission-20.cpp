02742a197425faab5346812b4a3ecc29d23aff53
AllenDiiaz <88265742+AllenDiiaz@users.noreply.github.com>
2026-07-20 21:03:54 +0800

    Add: eating-bananas - submission-20

---
+++ Data Structures & Algorithms/eating-bananas/submission-20.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+
+        while (left < right)
+        {
+            int mid = left + (right-left) / 2;
+            if (canFinish(piles,mid,h)){right = mid;}
+            else {left = mid + 1;}
+        }
+        return left;
+    }
+
+private:
+    bool canFinish(vector<int>& piles, int speed, int h) {
+
+        long long hours = 0;
+        for(int p: piles) 
+        {
+            hours += (p + speed - 1) / speed;
+            if (hours > h) return false;
+        }
+        return true;
+    }
+    
+};
