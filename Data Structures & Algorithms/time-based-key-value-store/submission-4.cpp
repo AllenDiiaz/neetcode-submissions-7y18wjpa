2cae209542295c3028ed0dc1be5c81e334076177
AllenDiiaz <88265742+AllenDiiaz@users.noreply.github.com>
2026-08-05 00:26:41 +0800

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.cpp
@@ -0,0 +1,40 @@
+class TimeMap {
+
+// "alice", "happy", 1
+private:
+unordered_map<string, vector<pair<string, int>>> store;
+
+public:
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        store[key].push_back({value, timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        if (store.find(key) == store.end())
+        {return "";}
+
+        // store[Allen] = arr = [["happy", 1],["joyful", 3],....]
+        vector<pair<string, int>> &arr = store[key];
+        int left = 0;
+        int right = arr.size() - 1;
+        string ans = "";
+
+        while (left<=right)
+        {
+            int mid = left + (right-left) / 2;
+
+            if (arr[mid].second <= timestamp)
+            {
+                ans = arr[mid].first;
+                left = mid + 1;
+            } else {
+                right = mid -1;
+            }
+        }
+
+        return ans;
+
+    }
+};
